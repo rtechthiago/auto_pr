3af3c1fac20df0b29be62ceddc065ef814879a98
Robson Thiago <rtech.thiago@gmail.com>
2024-10-12 21:04:36 -0300

    Feature: Add issue creation

--- .github/scripts/issue.py
+++ .github/scripts/issue.py
@@ -12,13 +12,7 @@ def create_github_issue(repo_name, issue_title, issue_body, token):
     issue = repo.create_issue(
         title=issue_title,
         body=issue_body,
-        issue_body = """
-        Descrição da Issue.
-        Por favor, selecione uma das seguintes opções para prosseguir:
-        - [ ] Opção 1
-        - [ ] Opção 2
-        - [ ] Opção 3
-        """
+
     )
 
     print(f"Issue criada com sucesso: {issue.html_url}")
@@ -27,7 +21,13 @@ if __name__ == "__main__":
     # Variáveis de ambiente
     repo_name = os.getenv('GITHUB_REPOSITORY')
     issue_title = os.getenv('ISSUE_TITLE', 'Automated Issue Title')
-    issue_body = os.getenv('ISSUE_BODY', 'This issue was created automatically by GitHub Actions.')
     github_token = os.getenv('GITHUB_TOKEN')
+    issue_body = """
+        Descrição da Issue.
+        Por favor, selecione uma das seguintes opções para prosseguir:
+        - [ ] Opção 1
+        - [ ] Opção 2
+        - [ ] Opção 3
+        """
 
     create_github_issue(repo_name, issue_title, issue_body, github_token)
